c51d716589979f1b8f68096b33c72134b3366ab2
Amit <khurjaamit@gmail.com>
2021-08-05 16:47:58 +0530

    create product and rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,60 +1,34 @@
-import data from './data.js'
+import data from "./data.js";
+import Product from "./components/Product.js";
 
 function App() {
-  return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            amazona
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">signin</a>
-        </div>
-      </header>
-
-      <main>
-        <div>
-          <div className="row center">
-            {data.products.map((product) => (
-              <div className="card">
-                <a href={`/product/${product._id}`}>
-                  <img className="medium" src={product.image} alt={product.name} />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  <div className="price">${product.price}</div>
+    return (
+        <div className="grid-container">
+            <header className="row">
+                <div>
+                    <a className="brand" href="/">
+                        amazona
+                    </a>
                 </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </main>
+                <div>
+                    <a href="/cart">Cart</a>
+                    <a href="/signin">signin</a>
+                </div>
+            </header>
 
-      <footer className="row center">copyright amitazadi</footer>
-    </div>
-  )
+            <main>
+                <div>
+                    <div className="row center">
+                        {data.products.map((product) => (
+                            <Product product={product}></Product>
+                        ))}
+                    </div>
+                </div>
+            </main>
+
+            <footer className="row center">copyright amitazadi</footer>
+        </div>
+    );
 }
 
-export default App
+export default App;
