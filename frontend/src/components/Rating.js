242081f87987e99fe03712f6295ff81ac99408d7
Amit <khurjaamit@gmail.com>
2021-08-05 22:01:30 +0530

    raitng and product component

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -4,7 +4,7 @@ function Rating(props) {
     return (
         <div className="rating">
             <span>
-                <i className={rating >= 1 ? "fa fa-star" : rating >= 0.5 ? "fa fa-star-half-o" : "fa fa-star-o"}></i>
+                <i className={rating >= 1 ? "fas fa fa-star " : rating >= 0.5 ? "fa fa-star-half-o" : "fa fa-star-o"}></i>
             </span>
             <span>
                 <i className={rating >= 2 ? "fa fa-star" : rating >= 1.5 ? "fa fa-star-half-o" : "fa fa-star-o"}></i>
